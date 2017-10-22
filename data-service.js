835607f3b681d18a9437217b3a76c38956ba72cc
Elsa Wai Chi Ng <26006006+elsa-ng@users.noreply.github.com>
2017-10-21 21:06:25 -0400

    initial commit

---
+++ data-service.js
@@ -0,0 +1,193 @@
+const fs = require("fs");
+
+var employees = [];
+var departments = [];
+
+// exports initialize() so it can be called from server.js
+// initializes the employees and departments array
+// returns a promise upon successful initialization
+module.exports.initialize = () => {
+
+    return new Promise((resolve, reject)=>{
+        fs.readFile("./data/employees.json", (err, data)=>{
+            if (err){
+                reject({message:"unable to read file"});
+            } else {
+                let empData = JSON.parse(data);
+                for (let x in empData){
+                    employees.push(empData[x]);
+                }
+                if (employees.length > 0){
+                    fs.readFile("./data/departments.json", (err, data)=>{
+                        if (err){
+                            reject({message:"unable to read file"});
+                        } else {
+                            let depData = JSON.parse(data);
+                            for (let y in depData){
+                                departments.push(depData[y]);
+                            }
+                            if (departments.length > 0){
+                                resolve();
+                            } else {
+                                reject({message:"unable to read file"});
+                            }
+                        }
+                    });
+                } else {
+                    reject({message:"unable to read file"});
+                }
+            }
+        });
+    });
+};
+
+// exports getAllEmployees() to be called from server.js
+// returns the employees array through a promise
+module.exports.getAllEmployees = () => {
+    return new Promise((resolve, reject)=>{
+        if (employees.length > 0){
+            resolve(employees);
+        } else {
+            reject({message:"no results returned"});
+        }
+    });
+};
+
+// exports getEmployeesByStatus() to be called from server.js
+// returns the results in an array through a promise
+module.exports.getEmployeesByStatus = (status) => {
+    return new Promise((resolve, reject)=>{
+        var empS = [];
+
+        if (employees.length > 0){
+            for (let j in employees){
+                if (employees[j].status == status){
+                    empS.push(employees[j]);
+                }
+            }
+
+            if (empS.length > 0){
+                resolve(empS);
+            } else {
+                reject({message:"no match is found"});
+            }
+
+        } else {
+            reject({message:"no results returned"});
+        }
+    });
+};
+
+// exports getEmployeesByDepartment() to be called from server.js
+// returns the results in an array through a promise
+module.exports.getEmployeesByDepartment = (department) => {
+    return new Promise((resolve, reject)=>{
+        var empD = [];
+
+        if (employees.length > 0){
+            for (let m in employees){
+                if (employees[m].department == department){
+                    empD.push(employees[m]);
+                }
+            }
+
+            if (empD.length > 0){
+                resolve(empD);
+            } else {
+                reject({message:"no match is found"});
+            }
+
+        } else {
+            reject({message:"no results returned"});
+        }
+    });
+};
+
+// exports getEmployeesByManager() to be called from server.js
+// returns the results in an array through a promise
+module.exports.getEmployeesByManager = (manager) => {
+    return new Promise((resolve, reject)=>{        
+        var empM = [];
+
+        if (employees.length > 0){
+            for (let n in employees){
+                if (employees[n].employeeManagerNum == manager){
+                    empM.push(employees[n]);
+                }
+            }
+
+            if (empM.length > 0){
+                resolve(empM);
+            } else {
+                reject({message:"no match is found"});
+            }            
+        
+        } else {
+            reject({message:"no results returned"});
+        }
+    });
+};
+
+// exports getEmployeeByNum() to be called from server.js
+// returns the result through a promise
+// part of the logic is taken from the standard solution obtained from Patrick
+module.exports.getEmployeeByNum = (num) => {
+    return new Promise ((resolve, reject)=>{
+        var foundEmployee = null; // taken from the standard solution
+        
+        if (employees.length > 0) {
+            for (let i in employees){
+                if (employees[i].employeeNum == num){
+                    foundEmployee = employees[i]; // taken from the standard solution
+                }
+            }
+            
+            if (!foundEmployee){ // taken from the standard solution
+                reject({message:"no match is found"});
+            } else {
+                resolve(foundEmployee);
+            }
+
+        } else {
+            reject({message: "no results returned"});
+        }
+        
+    });
+};
+
+// exports getManagers() to be called from server.js
+// returns the results in an array through a promise
+module.exports.getManagers = () => {
+    return new Promise((resolve, reject)=>{     
+        let  isMan = [];
+
+        if (employees.length > 0){
+            for (let k in employees){
+                if (employees[k].isManager == true){
+                    isMan.push(employees[k]);
+                }
+            }
+
+            if (isMan.length > 0){
+                resolve(isMan);
+            } else {
+                reject({message:"no match is found"});
+            }
+            
+        } else {
+            reject({message:"no results returned"});
+        }
+    });
+};
+
+// exports getDepartments() to be called from server.js
+// returns the departments array through a promise
+module.exports.getDepartments = () => {
+    return new Promise((resolve, reject)=>{
+        if (departments.length > 0){
+            resolve(departments);
+        } else {
+            reject({message:"no results returned"});
+        }
+    });
+};
---
+++ server.js
@@ -0,0 +1,119 @@
+/*********************************************************************************
+* WEB322 – Assignment 02
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part
+* of this assignment has been copied manually or electronically from any other source
+* (including 3rd party web sites) or distributed to other students.
+*
+* Name: Wai Chi Ng          Student ID: 140634163         Date: October 4, 2017
+*
+* Online (Heroku) Link: https://seneca-web322-wcng1.herokuapp.com/
+*
+********************************************************************************/ 
+
+const data_service = require("./data-service.js");
+const express = require("express");
+const path = require("path");
+const exphbs = require("express-handlebars");
+const bodyParser = require("body-parser");
+const app = express();
+const HTTP_PORT = process.env.PORT || 8080;
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.engine(".hbs", exphbs({
+    extname: ".hbs",
+    defaultLayout: 'layout',
+    helpers: {
+        equal: function(lvalue, rvalue, options){
+            if (arguments.length < 3){
+                throw new Error("Handlebars Helper equal needs 2 parameters");
+            }
+            if (lvalue != rvalue){
+                return options.inverse(this);
+            } else {
+                return options.fn(this);
+            }
+        }
+    }
+}));
+
+app.set("view engine", ".hbs");
+
+/* I updated my server.js using the logic from the standard solution
+   which I obtained from Patrick. After reading the standard solution,
+   I realized I overlooked one important factor which stemmed from my
+   misunderstanding and misreading Assignment 3's instructions.
+ */
+
+app.get("/", (req, res)=>{
+    res.render("home");
+});
+
+app.get("/about", (req, res)=>{
+    res.render("about");
+});
+
+app.get("/employees", (req, res)=>{
+    if (req.query.status){
+        data_service.getEmployeesByStatus(req.query.status).then((data)=>{
+            res.render("employeeList", {data: data, title: "Employees"});
+        }).catch((err)=>{
+            res.render("employeeList", {data: {}, title: "Employees"});
+        });
+    } else if (req.query.department){
+        data_service.getEmployeesByDepartment(req.query.department).then((data)=>{
+            res.json(data);
+        }).catch((err)=>{
+            res.json(err);
+        });
+    } else if (req.query.manager){
+        data_service.getEmployeesByManager(req.query.manager).then((data)=>{
+            res.json(data);
+        }).catch((err)=>{
+            res.json(err);
+        });
+    } else {
+        data_service.getAllEmployees().then((data)=>{
+            res.json(data);
+        }).catch((err)=>{
+            res.json(err);
+        });
+    }
+});
+
+app.get("/employee/:value", (req,  res)=>{
+    data_service.getEmployeeByNum(req.params.value).then((data)=>{
+        res.json(data);
+    }).catch((err)=>{
+        res.json(err);
+    });
+});
+
+app.get("/managers", (req, res)=>{
+    data_service.getManagers().then((data)=>{
+        res.render("employeeList", {data: data, title: "Employees (Managers)"});
+    }).catch((err)=>{
+        res.render("employeeList", {data: {}, title: "Employees (Managers)"});
+    });
+});
+
+app.get("/departments", (req, res)=>{
+    data_service.getDepartments().then((data)=>{
+        res.render("departmentList", {data: data, title: "Departments"});
+    }).catch((err)=>{
+        res.render("departmentList", {data: {}, title: "Departments"});
+    }); 
+});
+
+app.use((req, res)=>{
+    res.status(404).send("Page Not Found");
+});
+
+data_service.initialize().then(()=>{
+    app.listen(HTTP_PORT, () =>{
+        console.log("server listening on " + HTTP_PORT);
+    }); 
+}).catch((err)=>{
+    res.json(err);
+});
